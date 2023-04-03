d75805b8cdec6d471637f72ee9e117d5d5af8cc5
Wojciech Pakoca <wojtekpako2@gmail.com>
2023-04-03 18:06:48 +0200

    Metric Bmi Calculator

---
+++ BmiCalculator/BmiCalculator/Calculator/MetricBmiCalculator.cs
@@ -0,0 +1,18 @@
+﻿using BmiCalculator.Interface;
+
+namespace BmiCalculator.Calculator
+{
+    public class MetricBmiCalculator : IBmiCalculator
+    {
+        public double CalculateBmi(double weight, double height)
+        {
+            if (weight <= 0)
+                throw new ArgumentException("Weight is not a valid number");
+            if (height <= 0)
+                throw new ArgumentException("Height is not a valid number");
+
+            var bmi = weight / Math.Pow((height / 100), 2);
+            return Math.Round(bmi, 2);
+        }
+    }
+}
---
+++ BmiCalculator/BmiCalculator/Interface/IBmiCalculator.cs
@@ -0,0 +1,7 @@
+﻿namespace BmiCalculator.Interface
+{
+    public interface IBmiCalculator
+    {
+        public double CalculateBmi(double weight, double height);
+    }
+}
